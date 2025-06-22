cb90d35d914b9a5892230e6d415dc125d16d8215
Hyper <hypersoweak@gmail.com>
2025-06-22 18:18:02 +0800

    feat: basic enemy and spawner

---
+++ scripts/enemy.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export var speed := 100.0
+var path_follow: PathFollow2D
+
+func _ready():
+	path_follow = get_parent() as PathFollow2D
+
+func _process(delta):
+	path_follow.progress += speed * delta
+	if path_follow.progress_ratio >= 1.0:
+		queue_free()
---
+++ scripts/spawner.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+@export var enemy_scene: PackedScene
+@export var spawn_interval := 2.0
+@export var max_enemies := 20
+
+var enemies_spawned := 0
+
+@onready var spawn_timer: Timer = $Timer
+@onready var path: Path2D = $"../ComputerPath"
+@onready var enemies_container: Node2D = $"../Enemies"
+
+func _ready():
+	spawn_timer.wait_time = spawn_interval
+	spawn_timer.timeout.connect(_on_spawn_timeout)
+	spawn_timer.start()
+
+func _on_spawn_timeout():
+	if enemies_spawned >= max_enemies:
+		spawn_timer.stop()
+		return
+
+	var enemy = enemy_scene.instantiate()
+	var path_follow = PathFollow2D.new()
+	path.add_child(path_follow)
+	path_follow.progress_ratio = 0.0
+	path_follow.add_child(enemy)
+	
+	enemies_container.add_child(path_follow)
+	enemies_spawned += 1
