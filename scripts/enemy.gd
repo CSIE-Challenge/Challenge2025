a9043a7b0a057366aad89355381d59b91ea5b65c
Hyper <hypersoweak@gmail.com>
2025-06-24 03:17:32 +0800

    haha test precommit again

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,25 +1,17 @@
 extends CharacterBody2D
 
-@onready var health_bar := $HealthBar
 @export var speed := 300.0
-var health: int
-var path_follow: PathFollow2D
 @export var max_health := 100
+
+var path_follow: PathFollow2D
+var health: int
+
+@onready var health_bar := $HealthBar
 
 
 func _ready():
 	path_follow = get_parent() as PathFollow2D
 	health = max_health
-
-
-
-
-
-
-
-
-
-
 
 
 func _process(delta):
@@ -29,9 +21,13 @@ func _process(delta):
 
 	health_bar.rotation = -path_follow.rotation
 	health_bar.value = health / float(max_health) * 100.0
+
+
 func take_damage(amount: int):
 	health -= amount
 	if health <= 0:
 		die()
+
+
 func die():
 	queue_free()
