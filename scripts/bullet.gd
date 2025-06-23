469ad1cab58fb1cac25d51a0f7b25e13f6674309
mtmatt <mtmattqq@gmail.com>
2025-06-23 14:03:45 +0800

    feat: add bullet for tower

---
+++ scripts/bullet.gd
@@ -0,0 +1,17 @@
+extends StaticBody2D
+
+var damage: int = 10
+var speed: float = 400  # per second
+var direction: Vector2 = Vector2(0, 1)
+
+
+func _ready() -> void:
+    pass
+
+
+func _process(delta: float) -> void:
+    pass
+
+
+func _physics_process(delta: float) -> void:
+    position += delta * speed * direction
