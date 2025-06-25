dabd50ecb3968a4eefcd7fa254de1436a40dd3d0
mtmatt <mtmattqq@gmail.com>
2025-06-25 17:29:40 +0800

    chore: setup API Server and Agents

---
+++ scripts/APIServer/connection.gd
@@ -0,0 +1,90 @@
+class_name WebSocketConnection
+extends Node
+
+signal received_text(msg: String)
+signal client_connected
+signal client_disconnected
+
+const TOKEN_LEN: int = 4
+static var connection_id_counter = 101
+
+var id: int
+var _socket: WebSocketPeer
+var _token: String
+
+
+func _init() -> void:
+	id = connection_id_counter
+	connection_id_counter += 1
+	_token = _generate_token()
+	_socket = null
+	add_to_group("websocket_connections")
+
+
+func authenticate(token: String) -> bool:
+	return token == _token
+
+
+func connect_to_socket(socket: WebSocketPeer) -> void:
+	_socket = socket
+	client_connected.emit()
+
+
+func disconnect_from_socket() -> void:
+	if _socket != null:
+		var state = _socket.get_ready_state()
+		if state != WebSocketPeer.STATE_CLOSING and state != WebSocketPeer.STATE_CLOSED:
+			_socket.close()
+		if state == WebSocketPeer.STATE_CLOSED:
+			_socket = null
+			client_disconnected.emit()
+
+
+func is_client_connected() -> bool:
+	return _socket != null
+
+
+func send_text(msg: String) -> Error:
+	if _socket == null:
+		return ERR_UNAVAILABLE
+	return _socket.send_text(msg)
+
+
+func _get_message() -> Variant:
+	if !_has_message():
+		return null
+	var pkt: PackedByteArray = _socket.get_packet()
+	if _socket.was_string_packet():
+		return pkt.get_string_from_utf8()
+	return pkt
+
+
+func _has_message() -> bool:
+	return _socket != null and _socket.get_available_packet_count() > 0
+
+
+func _poll() -> void:
+	if _socket == null:
+		return
+	_socket.poll()
+	if _socket.get_ready_state() != WebSocketPeer.STATE_OPEN:
+		disconnect_from_socket()
+		return
+	while _has_message():
+		var recv = _get_message()
+		if typeof(recv) == TYPE_STRING:
+			received_text.emit(recv)
+		else:
+			print("Error: unknown msg type")
+
+
+func _process(_delta: float) -> void:
+	_poll()
+
+
+func _generate_token() -> String:
+	const CHARACTERS: String = "abcdefghijklmnopqrstuvwxyz"
+	var tok: String = ""
+	for i in range(TOKEN_LEN):
+		tok += CHARACTERS[randi_range(0, CHARACTERS.length() - 1)]
+	return tok
