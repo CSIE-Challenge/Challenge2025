e9828efab1a7a037243287e1e9d7d612fc295fe0
jameson931010 <jameson931010@gmail.com>
2025-06-25 16:58:43 +0800

    feat: display financial information on UI panel

--- scripts/attack.gd
+++ scripts/attack.gd
@@ -40,16 +40,7 @@ func switch_panel_mode():
 func _ready():
 	enemy_grid.custom_minimum_size = Vector2(300, 900)
 	for enemy in enemy_list:
-		var btn = TextureButton.new()
-		var tex = load(enemy_list[enemy].icon_path)
-		btn.texture_normal = tex
-		#btn.expand_icon = true
-		btn.stretch_mode = TextureButton.STRETCH_SCALE
-		btn.custom_minimum_size = Vector2(150, 100)
-		btn.name = enemy
-
-		btn.pressed.connect(_on_enemy_pressed.bind(enemy))
-		enemy_grid.add_child(btn)
+		_spawn_button(enemy)
 
 	self.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	panel.visible = false
@@ -71,3 +62,40 @@ func _on_close_button_pressed():
 
 func _on_open_button_pressed():
 	switch_panel_mode()
+
+
+func _spawn_button(enemy: String):
+	var btn = Button.new()
+	#btn.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	#btn.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	btn.custom_minimum_size = Vector2(150, 175)
+	btn.flat = true
+	btn.name = enemy
+
+	var vbox = VBoxContainer.new()
+	vbox.alignment = BoxContainer.ALIGNMENT_CENTER
+	vbox.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	vbox.size_flags_vertical = Control.SIZE_EXPAND_FILL
+
+	var tex = TextureRect.new()
+	tex.texture = load(enemy_list[enemy].icon_path)
+	tex.stretch_mode = TextureRect.STRETCH_SCALE
+	tex.custom_minimum_size = Vector2(150, 150)
+	vbox.add_child(tex)
+
+	var cost_label = Label.new()
+	cost_label.text = "Cost: %d" % enemy_list[enemy].cost
+	cost_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	cost_label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+	vbox.add_child(cost_label)
+
+	var income_label = Label.new()
+	income_label.text = "Income: %d" % enemy_list[enemy].income
+	income_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	income_label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+	vbox.add_child(income_label)
+
+	btn.add_child(vbox)
+
+	btn.pressed.connect(_on_enemy_pressed.bind(enemy))
+	enemy_grid.add_child(btn)
