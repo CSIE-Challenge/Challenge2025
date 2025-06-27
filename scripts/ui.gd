f454f5e4bba4b0f4e68196b415f9a331e77b3939
prairie2022 <anthonycho0912@gmail.com>
2025-06-28 01:10:52 +0800

    feat: grab focus after submit chat message

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -4,7 +4,12 @@ extends Node2D
 @onready var message_box = $MessageBox
 
 
-func _on_send_button_pressed() -> void:
-	message_box.text += str("P: ", chat_input.text, "\n")
+func _on_chat_input_text_submitted(new_text: String) -> void:
+	message_box.text += str("P: ", new_text, "\n")
 	chat_input.text = ""
 	message_box.scroll_vertical = message_box.get_line_count() - 1
+	chat_input.grab_focus()
+
+
+func _on_send_button_pressed() -> void:
+	_on_chat_input_text_submitted(chat_input.text)
