10a412670ef11367ff8b11c7c248d9db4c7cf73c
dj4zo6u.6 <liaojoy985@gmail.com>
2025-06-27 16:40:11 +0800

    feat: make  bullet disappear by time_to_live

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -2,18 +2,23 @@ extends Area2D
 
 @export var speed := 400.0
 @export var rotation_speed := 3.0
-@export var max_distance := 800.0
+@export var time_to_live := 5.0 # seconds
 @export var damage := 3
 
 var target: Node2D = null
 var start_position: Vector2
+var _start_time := 0.0
 
 
 func _ready() -> void:
 	start_position = global_position
 	body_entered.connect(Callable(self, "_on_body_entered"))
+	_start_time = Time.get_ticks_msec() / 1000.0  # Record start time in seconds
 
 
+func _elapsed_time() -> float:
+	return Time.get_ticks_msec() / 1000.0 - _start_time
+	
 func _process(delta):
 	if target == null or not is_instance_valid(target):
 		queue_free()
@@ -31,11 +36,12 @@ func _process(delta):
 	if traverse_distance >= 10.0:
 		self.z_index = 20
 
-	if traverse_distance >= max_distance:
+	if _elapsed_time() >= time_to_live:
 		queue_free()
 
 
 func _on_body_entered(body: Node2D) -> void:
+	print("Bullet travel time: %.3f seconds" % _elapsed_time())
 	body.take_damage(damage)
 	# print("Bullet Hit: ", body.name)
 	queue_free()
