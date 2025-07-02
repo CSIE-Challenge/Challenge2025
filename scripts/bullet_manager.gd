21035005334897197d5ffb1a9c5f8755dd459230
littlecube8152 <froakie20161113@gmail.com>
2025-07-02 18:00:45 +0800

    chore: update comments

--- scripts/bullet_manager.gd
+++ scripts/bullet_manager.gd
@@ -5,16 +5,7 @@ extends Node2D
 @export var max_distance := 800.0
 @export var damage := 3
 @export var signal_bus: SignalBus
-# class Bullet:
-# 	var target: Node2D = null
-# 	var origin: Vector2
-# 	var position := Vector2()
-# 	var orientation := 0.0
-# 	var area_rid: RID
-# 	var ci_rid: RID
-# 	var pending_removal: bool = false
 
-# RenderingServer expects references to be kept around
 var bullet_texture = preload(
 	"res://assets/kenney_tower-defense-top-down/PNG/Default size/towerDefense_tile297.png"
 )
@@ -26,7 +17,6 @@ var bullets: Dictionary[RID, Bullet] = {}
 func _on_create_bullet(bullet: Bullet):
 	bullet.init_context(get_canvas_item(), get_world_2d().get_space())
 	bullets[bullet.id] = bullet
-	# _init_bullet(origin, orientation, target)
 
 
 func _destroy_bullet(bullet: Bullet):
