8cdeda5a3b632ce0bb098b4b5b1daa7364559a93
Hyper <hypersoweak@gmail.com>
2025-07-04 11:15:16 +0800

    feat: score bar

--- scripts/round.gd
+++ scripts/round.gd
@@ -2,7 +2,8 @@ extends Control
 
 const GAME_DURATION = 180.0
 
-@onready var game_timer_label := $Screen/Bottom/Mid/GameStatus/VBoxContainer/Countdown
+@export var game_timer_label: Label
+@export var score_bar: ProgressBar
 
 
 func _ready() -> void:
@@ -18,5 +19,13 @@ func get_formatted_time() -> String:
 	return "%02d:%02d" % [minutes, seconds]
 
 
+func _update_score_bar() -> void:
+	var p1_score = 1000
+	var p2_score = 800
+	var score = p1_score + p2_score
+	score_bar.value = float(p1_score) / score * 100.0
+
+
 func _process(_delta: float) -> void:
 	game_timer_label.text = get_formatted_time()
+	_update_score_bar()
