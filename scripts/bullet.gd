4b135b50955a6406a584d6ed1b645c6642be1165
PixelCat31415 <jimmychiu31415@gmail.com>
2025-07-04 11:26:06 +0800

    refactor: created related properties in bullets

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,14 +1,21 @@
 class_name Bullet
 extends Area2D
 
-@export var speed := 400.0
-@export var rotation_speed := 30.0
-@export var max_distance := 800.0
+
+@export var is_tracking: bool = false
+@export var is_anti_air: bool = false
+@export var is_penetrating: bool = false
+@export var aoe_radius: float = 0
+@export var movement_speed := 400.0
+@export var rotation_speed := 3.0
 @export var damage := 3
+@export var lifespan_seconds := 5
+
 
 var target: Node2D = null
 var start_position: Vector2
-var alive: bool = true
+var alive: bool = true  # make sure each bullet deals damage once
+var timer = Timer.new()
 
 
 func init(origin, orientation, _target) -> void:
@@ -17,23 +24,24 @@ func init(origin, orientation, _target) -> void:
 	rotation = orientation
 	target = _target
 
+	self.add_child(timer)
+	timer.timeout.connect(self.queue_free)
+	timer.start(lifespan_seconds)
+
 
 func _process(delta):
-	if target == null or not is_instance_valid(target):
-		queue_free()
-		return
+	var desired_angle: float
 
-	var dir = (target.global_position - global_position).normalized()
-	var desired_angle = dir.angle()
+	if target == null or not is_instance_valid(target):
+		desired_angle = rotation
+	else:
+		desired_angle = (target.global_position - global_position).angle()
 
 	rotation = lerp_angle(rotation, desired_angle, rotation_speed * delta)
-	position += Vector2.RIGHT.rotated(rotation) * speed * delta
+	position += Vector2.RIGHT.rotated(rotation) * movement_speed * delta
 
 	var traverse_distance = global_position.distance_to(start_position)
 
 	# The bullet must be above the tower after it is fired
 	if traverse_distance >= 10.0:
 		self.z_index = 20
-
-	if traverse_distance >= max_distance:
-		queue_free()
