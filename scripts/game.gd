e64f0971c63802b46a879972bb03b7bb203f1588
lemonilemon <imlemonilemon@gmail.com>
2025-07-07 04:31:49 +0800

    fix: fix linting

--- scripts/game.gd
+++ scripts/game.gd
@@ -18,6 +18,7 @@ var income_per_second = 10
 var income_rate: float = 1
 var built_towers: Dictionary = {}
 var previewer: Previewer = null
+var spell_dict: Dictionary
 var _enemy_scene_cache = {}
 
 @onready var _map: Map = $Map
@@ -154,8 +155,6 @@ func _deploy_enemy(enemy: Enemy, source: EnemySource) -> void:
 #endregion
 
 #region Spells
-
-var spell_dict: Dictionary
 
 
 func init_spells() -> void:
