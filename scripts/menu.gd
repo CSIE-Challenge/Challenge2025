81ac7e45b0f1000c009457c0b7d2133476d9ce1f
Hyper <hypersoweak@gmail.com>
2025-07-10 01:56:02 +0800

    feat: adjust menu screen

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,6 +1,10 @@
 extends Control
 
 
+func _ready() -> void:
+	$Version.text = "v%s" % [ProjectSettings.get_setting("application/config/version")]
+
+
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/player_selection/player_selection.tscn")
 
