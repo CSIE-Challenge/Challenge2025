ef052cb044a6aa8bff91edc04555ec518db820ae
PixelCat31415 <jimmychiu31415@gmail.com>
2025-07-09 16:29:26 +0800

    feat: switch to player selection from menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/round.tscn")
+	get_tree().change_scene_to_file("res://scenes/player_selection/player_selection.tscn")
 
 
 func _on_about_pressed() -> void:
