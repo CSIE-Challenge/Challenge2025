d3df7e2b1bd2d437b895b154e4f6488753e32cb2
littlecube8152 <froakie20161113@gmail.com>
2025-07-10 20:14:13 +0800

    fix: shop button will not resize when chat is default option

--- ui/shop_item.gd
+++ ui/shop_item.gd
@@ -6,17 +6,18 @@ var display_scene: PackedScene
 var display_cost: int = 10
 var display_name: String = ""
 
-@onready var container = $CenterContainer/Control
-
 
 func _ready():
-	await get_tree().process_frame
-	custom_minimum_size = Vector2(size.x, size.x)
 	self.gui_input.connect(_on_gui_input)
 	$CenterContainer/Control.add_child(display_scene.instantiate())
 	$Cost.text = "$%d" % display_cost
 	if not display_name.is_empty():
 		$Name.text = display_name
+
+
+func _process(_delta):
+	# Shouldn't be a performance issue here...
+	custom_minimum_size = Vector2(size.x, size.x)
 
 
 func _on_gui_input(event: InputEvent) -> void:
