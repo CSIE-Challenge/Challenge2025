825ce5d7379e5b94fac1f01986bdcbe81784b04d
prairie2022 <anthonycho0912@gmail.com>
2025-07-10 17:17:33 +0800

    fix: poison_effect
    trigger anytime within duration instead of trigger once

--- scripts/spells/poison_effect.gd
+++ scripts/spells/poison_effect.gd
@@ -3,7 +3,6 @@ extends Area2D
 @onready var spell: PoisonSpell = get_parent()
 @onready var collision: CollisionShape2D = $CollisionShape2D
 @onready var duration_timer: Timer = $DurationTimer
-@onready var trigger_timer: Timer = $TriggerTimer
 
 
 func _ready():
@@ -11,25 +10,16 @@ func _ready():
 	# Create duration timer
 	duration_timer.timeout.connect(_on_duration_ended)
 	duration_timer.wait_time = spell.metadata.stats.duration
-	# Create trigger timer
-	trigger_timer.timeout.connect(_on_trigger)
-	trigger_timer.wait_time = spell.metadata.stats.trigger_interval
 
 
 func _draw():
 	draw_circle(Vector2.ZERO, spell.metadata.stats.radius, Color.BLUE)
 
 
-func trigger() -> void:
-	# TODO: trigger damage anytime instead of only at the end
-	var enemies: Array[Area2D] = get_overlapping_areas()
-	for enemy in enemies:
-		enemy.take_damage(spell.metadata.stats.trigger_damage)
-
-
-func _on_trigger():
-	trigger()
-
-
 func _on_duration_ended():
 	queue_free()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("enemies"):
+		area.take_damage(spell.metadata.stats.trigger_damage)
