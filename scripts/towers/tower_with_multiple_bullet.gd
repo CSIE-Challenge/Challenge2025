08876cbe5419909ede515913d9d902f230227757
jameson931010 <jameson931010@gmail.com>
2025-07-11 18:49:38 +0800

    fix: align the animation with attacks

--- scripts/towers/tower_with_multiple_bullet.gd
+++ scripts/towers/tower_with_multiple_bullet.gd
@@ -18,6 +18,7 @@ func _ready():
 	enemy_detector.shape.radius = 0.5 * aim_range
 	shooting_timer = Timer.new()
 	self.add_child(shooting_timer)
+	#print(sprite.sprite_frames.get_frame_count(sprite.animation))
 
 
 func _flip_sprite() -> void:
@@ -29,10 +30,15 @@ func _flip_sprite() -> void:
 func _on_reload_timer_timeout() -> void:
 	_refresh_target()
 	if target == null:
-		anime.stop()
 		return
 	if not anime.is_playing():
 		anime.play("default")
+	var attack_scene = sprite.sprite_frames.get_frame_count(sprite.animation) - 1
+	wait_for_animation_timer.timeout.connect(self._on_fire_bullet, CONNECT_ONE_SHOT)
+	wait_for_animation_timer.start(ANIMATION_FRAME_DURATION * attack_scene)
+
+
+func _on_fire_bullet() -> void:
 	var bullet_to_shoot: int = roundi(randfn(expected_bullet_number, 3) + 0.5)
 	_spawn_bullet(bullet_to_shoot, SHOOTING_DURATION / bullet_to_shoot)
 
@@ -40,11 +46,10 @@ func _on_reload_timer_timeout() -> void:
 func _spawn_bullet(bullet_left: int, shooting_interval: float) -> void:
 	_refresh_target()
 	if target == null:
-		anime.stop()
 		return
 	var origin: Vector2 = $Tower/Marker2D.global_position
 	var direction: float = (
-		(target.global_position - origin).angle() + randfn(0.0, scattering_angle / 6)
+		(target.global_position - origin).angle() + randfn(0.0, scattering_angle / 2)
 	)
 	var bullet := bullet_scene.instantiate()
 	self.get_parent().add_child(bullet)
