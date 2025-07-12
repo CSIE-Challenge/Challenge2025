7d524936bff4e635f368d783eb7d8690756fe6c1
Hyper <hypersoweak@gmail.com>
2025-07-13 07:46:16 +0800

    feat: volume settings

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -29,3 +29,13 @@ func _on_resume_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_settings_pressed() -> void:
+	$PauseMenu.visible = false
+	$Settings.visible = true
+
+
+func _on_back_pressed() -> void:
+	$PauseMenu.visible = true
+	$Settings.visible = false
