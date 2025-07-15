b6c5f349733d6492919901a38b2a2d6f4f5b6541
PixelCat31415 <jimmychiu31415@gmail.com>
2025-07-15 17:50:51 +0800

    feat: added option to auto-restart agent processes

--- scripts/agents/python_subprocess_manager.gd
+++ scripts/agents/python_subprocess_manager.gd
@@ -3,16 +3,28 @@ extends Node
 
 enum { NEVER_RUN, RUNNING, KILLED, EXITED, FAILED }
 
+# waiting time from this termination to next auto-restart
+const RESTART_INTERVAL_SEC: float = 1.0
+
 @export var python_interpreter_path: String = ""
 @export var python_script_path: String = ""
+@export var auto_restart: bool = false
 
 var _state: int = NEVER_RUN
 var _current_pid: int
 var _current_stdio_pipe: FileAccess
 var _last_exit_code: int
+var _restart_timer: Timer
+var _ran_after_setting_auto_restart: bool = false
 
 
 func _init() -> void:
+	_restart_timer = Timer.new()
+	add_child(_restart_timer)
+	_restart_timer.one_shot = true
+	_restart_timer.wait_time = RESTART_INTERVAL_SEC
+	_restart_timer.timeout.connect(run_subprocess)
+
 	OS.set_environment("IS_CHALLENGE_GAME_PROCESS", "TRUE")
 	# keep monitoring the subprocess when the game is paused
 	process_mode = Node.PROCESS_MODE_ALWAYS
@@ -24,6 +36,13 @@ func set_python_interpreter(path: String) -> void:
 
 func set_python_script(path: String) -> void:
 	python_script_path = path
+
+
+func set_auto_restart(value: bool) -> void:
+	auto_restart = value
+	if not auto_restart:
+		_restart_timer.stop()
+		_ran_after_setting_auto_restart = false
 
 
 func get_state() -> int:
@@ -55,10 +74,15 @@ func is_runnable() -> bool:
 	)
 
 
+func is_restarting() -> bool:
+	return not _restart_timer.is_stopped()
+
+
 # start the subprocess and send the token via stdin
 func run_subprocess() -> void:
 	if not is_runnable():
 		return
+	_restart_timer.stop()
 	_current_pid = OS.create_process(python_interpreter_path, [python_script_path])
 	if _current_pid == -1:
 		_state = FAILED
@@ -68,21 +92,28 @@ func run_subprocess() -> void:
 
 # terminate the subprocess
 func kill_subprocess() -> void:
+	set_auto_restart(false)
+	_restart_timer.stop()
 	if not is_running():
 		return
 	_state = KILLED
 	OS.kill(_current_pid)
 
 
-# close the stdio pipes once we get responses from stdout pipe
-# check if the subprocess exited and collect exit code
 func _process(_delta: float) -> void:
 	if _state != RUNNING and _state != KILLED:
+		# process is not running and is not killed, possibly runnable
+		if _ran_after_setting_auto_restart and auto_restart and not is_restarting():
+			_restart_timer.start()
 		return
 	if OS.is_process_running(_current_pid):
+		if auto_restart:
+			_ran_after_setting_auto_restart = true
+		# close the stdio pipes once we get responses from stdout pipe
 		if _current_stdio_pipe != null and _current_stdio_pipe.get_length() > 0:
 			_current_stdio_pipe = null
 		return
+	# collect exit code if the subprocess has terminated
 	_state = EXITED
 	_last_exit_code = OS.get_process_exit_code(_current_pid)
 
