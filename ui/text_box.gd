c094b6816901ce8904e02255af0735fe121bcf45
bbwinner <bbcam7392@gmail.com>
2025-07-15 13:01:53 +0800

    Merge feat/add-chats

--- ui/text_box.gd
+++ ui/text_box.gd
@@ -3,3 +3,12 @@ extends MarginContainer
 
 func set_text(text: String):
 	$MarginContainer/RichTextLabel.text = text
+	if "I use Arch, btw." in text:
+		$MarginContainer/LinkButton.set_uri("https://wiki.archlinux.org/title/Main_page")
+
+
+func _on_link_button_pressed() -> void:
+	print($MarginContainer/LinkButton.uri)
+
+	#match $MarginContainer/RichTextLabel.text:
+	#"I use Arch, btw."
