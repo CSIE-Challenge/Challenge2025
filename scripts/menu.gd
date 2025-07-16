d0da23a9b77f69ed960b6afec39cc581651ecdc1
PixelCat31415 <jimmychiu31415@gmail.com>
2025-07-16 11:56:06 +0800

    feat: enter quad loading screen from home page

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -36,6 +36,11 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/player_selection/player_selection.tscn")
 
 
+func _on_quad_pressed() -> void:
+	AudioManager.button_on_click.play()
+	get_tree().change_scene_to_file("res://scenes/quad/quad_match_loader.tscn")
+
+
 func _on_about_pressed() -> void:
 	AudioManager.button_on_click.play()
 	get_tree().change_scene_to_file("res://scenes/about.tscn")
