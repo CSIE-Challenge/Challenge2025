bd4290661fb8aaba74fd43d8828168129481ca78
Andromeda0105 <0501dylan.andromeda@gmail.com>
2025-07-17 16:14:38 +0800

    feat: add shadow for danmaku

--- scripts/curtain.gd
+++ scripts/curtain.gd
@@ -13,3 +13,19 @@ func setup(text: String, font_size := 24, color := Color.WHITE):
 	var font = FontFile.new()
 	add_theme_font_size_override("font_size", font_size)
 	add_theme_color_override("font_color", color)
+
+	var bg = StyleBoxFlat.new()
+	bg.bg_color = Color(0, 0, 0, 0.4)
+	bg.corner_radius_top_left = 6
+	bg.corner_radius_top_right = 6
+	bg.corner_radius_bottom_left = 6
+	bg.corner_radius_bottom_right = 6
+
+	bg.content_margin_left = 8
+	bg.content_margin_right = 8
+	bg.content_margin_top = 4
+	bg.content_margin_bottom = 4
+
+	add_theme_stylebox_override("normal", bg)
+	await self.ready
+	self.size = self.get_minimum_size()
