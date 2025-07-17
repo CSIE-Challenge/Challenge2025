e454351c44582d80f5f4b8ed71fbe32f56d12059
jameson931010 <jameson931010@gmail.com>
2025-07-18 01:19:27 +0800

    refactor: rename direction

--- scripts/towers/fort.gd
+++ scripts/towers/fort.gd
@@ -1,6 +1,8 @@
 class_name Fort
 extends Tower
 
+enum Direction { LEFT, RIGHT, UPPER, LOWER }
+
 var target_direction: float
 var marker: Marker2D
 var animation: StringName = "LeftRight"  # Default scene
@@ -10,21 +12,27 @@ var _map: Map
 func enable(_global_position: Vector2, map: Map) -> void:
 	global_position = _global_position
 	_map = map
-	target_direction = _get_fort_direction()
-	if sin(target_direction) < -0.5:
-		marker = $Tower/UpperMarker
-		animation = "Upper"
-	elif sin(target_direction) > 0.5:
-		marker = $Tower/LowerMarker
-		animation = "Lower"
-	else:
-		marker = $Tower/LeftRightMarker
-		animation = "LeftRight"
-		sprite.flip_h = cos(target_direction) < 0
-		if sprite.flip_h:
-			marker.position.x = -abs(marker.position.x)
-		else:
+	var dir = _get_fort_direction()
+	match dir:
+		Direction.LOWER:
+			marker = $Tower/UpperMarker
+			animation = "Upper"
+			target_direction = -PI / 2
+		Direction.UPPER:
+			marker = $Tower/LowerMarker
+			animation = "Lower"
+			target_direction = PI / 2
+		Direction.RIGHT:
+			marker = $Tower/LeftRightMarker
+			animation = "LeftRight"
 			marker.position.x = abs(marker.position.x)
+			target_direction = 0
+		Direction.LEFT:
+			marker = $Tower/LeftRightMarker
+			animation = "LeftRight"
+			sprite.flip_h = true
+			marker.position.x = -abs(marker.position.x)
+			target_direction = -PI
 	sprite.set_animation(animation)
 	reload_timer.start(reload_seconds)
 
@@ -50,7 +58,7 @@ func _on_fire_bullet() -> void:
 	bullet.init(origin, direction, target, damage)
 
 
-func _get_fort_direction() -> float:
+func _get_fort_direction() -> Direction:
 	var direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 	var best_direction = PI
 	var best_count = 0
@@ -67,5 +75,12 @@ func _get_fort_direction() -> float:
 			terrain_type = _map.get_cell_terrain(Vector2i(cur_x, cur_y))
 		if count > best_count:
 			best_count = count
-			best_direction = PI * min(dir[0], 0) + PI / 2 * dir[1]
-	return best_direction
+			best_direction = dir
+	match best_direction:
+		[1, 0]:
+			return Direction.RIGHT
+		[-1, 0]:
+			return Direction.LEFT
+		[0, 1]:
+			return Direction.UPPER
+	return Direction.LOWER
