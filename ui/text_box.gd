d9965ecb30f4bb5f6509ba639d00b489d8bad0f8
Leo.Pishiou.Chang <pishiou.chang@gmail.com>
2025-07-17 12:50:46 +0800

    fix: line length error

--- ui/text_box.gd
+++ ui/text_box.gd
@@ -8,7 +8,8 @@ func set_text(text: String):
 	elif "Ruby 醬，嗨，你喜歡什麼？" in text:
 		$MarginContainer/LinkButton.set_uri("https://www.instagram.com/reel/DI00i2FSkfj/")
 	elif "d_ouo_b_" in text:
-		$MarginContainer/LinkButton.set_uri("https://www.instagram.com/d_ouo_b_?igsh=MWcyenNwZjNibGI4dA%3D%3D&utm_source=qr")
+		$MarginContainer/LinkButton.set_uri("https://www.instagram.com/\
+d_ouo_b_?igsh=MWcyenNwZjNibGI4dA%3D%3D&utm_source=qr")
 
 
 func set_line_height(send_pixelcat: bool):
