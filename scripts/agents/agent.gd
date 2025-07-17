609c8be65d21980434ac72be03e0b3932b803313
mtmatt <mtmattqq@gmail.com>
2025-07-17 17:42:18 +0800

    feat: enable the boo feature

--- scripts/agents/agent.gd
+++ scripts/agents/agent.gd
@@ -573,16 +573,14 @@ func _set_name(_name: String) -> Array:
 	return [StatusCode.OK]
 
 
-# Please don't activate this until sometime near deadline
 func boo() -> Array:
-	return [StatusCode.COMMAND_ERR, "Not yet implemented"]
-	#var value
-	#if randf() < 0.95:
-	#value = randi_range(-10, 0)
-	#game_self.on_boo_called(value)
-	#return [StatusCode.OK, false]
-	#else:
-	#value = randi_range(400, 440)
-	#game_self.on_boo_called(value)
-	#return [StatusCode.OK, true]
+	var value: int
+	if randf() < 0.95:
+		value = randi_range(-10, 0)
+		game_self.on_boo_called(value)
+		return [StatusCode.OK, false]
+	value = randi_range(400, 440)
+	game_self.on_boo_called(value)
+	return [StatusCode.OK, true]
+
 #endregion
