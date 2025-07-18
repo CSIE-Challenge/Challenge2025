1fa6a2eea527d2ad8ac9ff845ef347e8f31f6cc8
littlecube8152 <froakie20161113@gmail.com>
2025-07-18 13:48:21 +0800

    fix: revert divide on exit area

--- scripts/maps/minecraft/map.gd
+++ scripts/maps/minecraft/map.gd
@@ -11,9 +11,8 @@ func _on_area_2d_area_entered(enemy: Area2D) -> void:
 		enemy.max_speed *= 2
 
 
-func _on_area_2d_area_exited(enemy: Area2D) -> void:
-	if "max_speed" in enemy:
-		enemy.max_speed /= 2
+func _on_area_2d_area_exited(_enemy: Area2D) -> void:
+	pass
 
 
 func _on_regen_timer_timeout() -> void:
