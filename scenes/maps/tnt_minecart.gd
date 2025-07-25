e49a62eab0063a37098812c3527d422da1243516
mtmatt <mtmattqq@gmail.com>
2025-07-25 17:08:32 +0800

    fix: formater

--- scenes/maps/tnt_minecart.gd
+++ scenes/maps/tnt_minecart.gd
@@ -8,6 +8,7 @@ var end: float
 
 @onready var boom_scene = preload("res://scenes/maps/boom.tscn")
 
+
 func _process(delta: float) -> void:
 	position.x += speed * delta
 	if position.x > end and visible:
@@ -17,18 +18,15 @@ func _process(delta: float) -> void:
 
 func explode():
 	var enemies: Array[Area2D] = $Area2D.get_overlapping_areas()
-	
+
 	for e in enemies:
 		if e.collision_layer & LAYER_9_BIT != 0:
 			var cell_pos = map.global_to_cell(e.global_position)
 			map.game.demolish_tower.emit(cell_pos)
 		elif e.has_method("take_damage"):
 			e.take_damage(1000000)
-	
+
 	for i in range(10):
 		var boom = boom_scene.instantiate()
-		boom.position = position + Vector2(
-			randf_range(-120, 120),
-			randf_range(-120, 120)
-		)
+		boom.position = position + Vector2(randf_range(-120, 120), randf_range(-120, 120))
 		map.add_child(boom)
